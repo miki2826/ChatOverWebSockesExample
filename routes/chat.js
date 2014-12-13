408e320e1e25e20d769a033f1c8fceadcb95174f
mikid <miki28226@gmail.com>
2014-12-13 12:01:49 +0200

    Initial commit

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
-var html_dir = path.join(__dirname, '../views/');
 router.get('/', function (req, res) {
     //TODO: understand query param and decide technology
     var socket = req.query && req.query.st;
